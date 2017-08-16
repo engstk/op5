b60e2ce34e4f9a444da2c7900741a3bb589d606e
andip71 <andreasp@gmx.de>
2017-08-16 13:50:31 +0200

    mdss_fb: add lcd backlight min limit
    Intention is mainly to limit the minimum brightness for the lcd backlight
    as OP5 auto brightness handling seems a bit too aggressive when it gets dark.
    
    [@nathanchance: Adjust permissions and remove max limit]
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- drivers/video/fbdev/msm/mdss_fb.c
+++ drivers/video/fbdev/msm/mdss_fb.c
@@ -86,6 +86,9 @@
  */
 #define MDP_TIME_PERIOD_CALC_FPS_US	1000000
 
+int backlight_min = 0;
+module_param(backlight_min, int, 0644);
+
 static struct fb_info *fbi_list[MAX_FBI_LIST];
 static int fbi_list_index;
 
@@ -290,6 +293,10 @@ static void mdss_fb_set_bl_brightness(struct led_classdev *led_cdev,
 
 	if (value > mfd->panel_info->brightness_max)
 		value = mfd->panel_info->brightness_max;
+
+	// Boeffla: apply min limits for LCD backlight (0 is exception for display off)
+	if (value != 0 && value < backlight_min)
+		value = backlight_min;
 
 	/* This maps android backlight level 0 to 255 into
 	   driver backlight level 0 to bl_max with rounding */
