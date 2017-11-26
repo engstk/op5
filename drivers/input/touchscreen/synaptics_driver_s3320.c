1f3bf508bee5a5b657b2bbe50996f18666708cb2
engstk <eng.stk@sapo.pt>
2017-11-26 04:20:59 +0000

    drivers: fix a couple leftover build errors
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- drivers/input/touchscreen/synaptics_driver_s3320.c
+++ drivers/input/touchscreen/synaptics_driver_s3320.c
@@ -1207,7 +1207,7 @@ static void gesture_judge(struct synaptics_ts_data *ts)
 	}
 
 	keyCode = UnkownGestrue;
-	/* Get key code based on registered gesture */
+	// Get key code based on registered gesture
 	switch (gesture) {
 	case DouTap:
 		keyCode = KEY_DOUBLE_TAP;
