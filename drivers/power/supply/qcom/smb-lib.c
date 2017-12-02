f856ae5d0bf8dcf98f88411510e64970813cb4d9
codeworkx <daniel.hillenbrand@codeworkx.de>
2017-12-02 21:17:10 +0100

    smb_lib: auto toggle otg switch based on dash status
    Auto enable otg when dash is absent, auto disable otg when dash is present.
    
    Change-Id: I4f98a545e37c2de395ca1db5874c86acd28e0bcd
    
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- drivers/power/supply/qcom/smb-lib.c
+++ drivers/power/supply/qcom/smb-lib.c
@@ -5048,6 +5048,8 @@ static void set_prop_batt_health(struct smb_charger *chg, int batt_health)
 static void set_usb_switch(struct smb_charger *chg, bool enable)
 {
 	int retrger_time;
+	const union power_supply_propval otg_on = {1,};
+	const union power_supply_propval otg_off = {0,};
 
 	if (!fast_charger) {
 		pr_err("no fast_charger register found\n");
@@ -5056,6 +5058,7 @@ static void set_usb_switch(struct smb_charger *chg, bool enable)
 
 	if (enable) {
 		pr_err("switch on fastchg\n");
+		op_set_prop_otg_switch(chg, &otg_off);
 		if (chg->boot_usb_present && chg->re_trigr_dash_done) {
 			vote(chg->usb_icl_votable, AICL_RERUN_VOTER,
 					true, 0);
@@ -5079,6 +5082,7 @@ static void set_usb_switch(struct smb_charger *chg, bool enable)
 		pr_err("switch off fastchg\n");
 		usb_sw_gpio_set(0);
 		mcu_en_gpio_set(1);
+		op_set_prop_otg_switch(chg, &otg_on);
 	}
 }
 
