c8aefb03c2d6c3a2266a5f4cb0aed1254cdf6bd4
Yaroslav Furman <yaro330@gmail.com>
2018-08-28 15:46:26 +0300

    power: main: silence logger
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- drivers/base/power/main.c
+++ drivers/base/power/main.c
@@ -129,8 +129,8 @@ void device_pm_add(struct device *dev)
 	device_pm_check_callbacks(dev);
 	mutex_lock(&dpm_list_mtx);
 	if (dev->parent && dev->parent->power.is_prepared)
-		dev_warn(dev, "parent %s should not be sleeping\n",
-			dev_name(dev->parent));
+		pr_debug("%s: parent %s should not be sleeping\n",
+			dev, dev_name(dev->parent));
 	list_add_tail(&dev->power.entry, &dpm_list);
 	mutex_unlock(&dpm_list_mtx);
 }
