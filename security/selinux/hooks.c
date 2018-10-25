7416ff5788c35f8152ab5617de631283f19cf45d
kdrag0n <dragon@khronodragon.com>
2018-10-24 19:09:01 -0700

    selinux: don't require auditing
    Audit is bad. It affects performance under all circumstances. It's also
    dirty.
    
    From LWN: "Andy submitted a patch to fix this particular problem, but he
    didn't stop there. He has come to the conclusion that the audit
    subsystem is beyond repair, so his patch marks the whole thing as being
    broken, making it generally inaccessible. He cited a number of problems
    beyond this security issue: it hurts performance even when it is not
    being used, it is not (in his mind) reliable, it has problems with
    various architectures, and "its approach to freeing memory is
    terrifying." All told, Andy said, we're better off without it"
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -2879,6 +2879,7 @@ static noinline int audit_inode_permission(struct inode *inode,
 					   int result,
 					   unsigned flags)
 {
+#ifdef CONFIG_AUDIT
 	struct common_audit_data ad;
 	struct inode_security_struct *isec = inode->i_security;
 	int rc;
@@ -2890,6 +2891,7 @@ static noinline int audit_inode_permission(struct inode *inode,
 			    audited, denied, result, &ad, flags);
 	if (rc)
 		return rc;
+#endif
 	return 0;
 }
 
