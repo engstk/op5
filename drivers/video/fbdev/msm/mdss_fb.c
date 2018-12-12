75941f7f01fc73630248cf93258e0996c3455988
Marko Man <darkobas@gmail.com>
2018-12-12 11:20:55 +0100

    fb: strip text from panel modes sysfs
    Change-Id: I19836af4b8f88f29f621ff0b3ee32bc3dccab439
    
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- drivers/video/fbdev/msm/mdss_fb.c
+++ drivers/video/fbdev/msm/mdss_fb.c
@@ -897,10 +897,7 @@ static ssize_t mdss_fb_get_srgb_mode(struct device *dev,
 
 	level = mdss_fb_send_panel_event(mfd, MDSS_EVENT_PANEL_GET_SRGB_MODE,
 			NULL);
-	ret = scnprintf(buf, PAGE_SIZE, "mode = %d\n"
-							"0-->sRGB Mode OFF\n"
-							"1-->sRGB Mode ON\n",
-							level);
+	ret=scnprintf(buf, PAGE_SIZE, "%d\n", level);
 	return ret;
 }
 
@@ -939,10 +936,7 @@ static ssize_t mdss_fb_get_adobe_rgb_mode(struct device *dev,
 
 	level = mdss_fb_send_panel_event(mfd,
 		MDSS_EVENT_PANEL_GET_ADOBE_RGB_MODE, NULL);
-	ret = scnprintf(buf, PAGE_SIZE, "mode = %d\n"
-					"0-->Adobe RGB Mode OFF\n"
-					"1-->Adobe RGB Mode ON\n",
-					level);
+	ret=scnprintf(buf, PAGE_SIZE, "%d\n", level);
 	return ret;
 }
 
@@ -980,10 +974,7 @@ static ssize_t mdss_fb_get_dci_p3_mode(struct device *dev,
 
 	level = mdss_fb_send_panel_event(mfd, MDSS_EVENT_PANEL_GET_DCI_P3_MODE,
 			NULL);
-	ret = scnprintf(buf, PAGE_SIZE, "mode = %d\n"
-						"0-->DCI-P3 Mode OFF\n"
-						"1-->DCI-P3 Mode ON\n",
-						level);
+	ret=scnprintf(buf, PAGE_SIZE, "%d\n", level);
 	return ret;
 }
 
@@ -1021,10 +1012,7 @@ static ssize_t mdss_fb_get_night_mode(struct device *dev,
 
 	level = mdss_fb_send_panel_event(mfd, MDSS_EVENT_PANEL_GET_NIGHT_MODE,
 			NULL);
-	ret = scnprintf(buf, PAGE_SIZE, "mode = %d\n"
-							"0-->Night Mode OFF\n"
-							"1-->Night Mode ON\n",
-							level);
+	ret=scnprintf(buf, PAGE_SIZE, "%d\n", level);
 	return ret;
 }
 
@@ -1062,10 +1050,7 @@ static ssize_t mdss_fb_get_oneplus_mode(struct device *dev,
 
 	level = mdss_fb_send_panel_event(mfd, MDSS_EVENT_PANEL_GET_ONEPLUS_MODE,
 			NULL);
-	ret = scnprintf(buf, PAGE_SIZE, "mode = %d\n"
-						"0-->Oneplus Mode OFF\n"
-						"1-->Oneplus Mode ON\n",
-						level);
+	ret=scnprintf(buf, PAGE_SIZE, "%d\n", level);
 	return ret;
 }
 
@@ -1103,10 +1088,7 @@ static ssize_t mdss_fb_get_adaption_mode(struct device *dev,
 
 	level = mdss_fb_send_panel_event(mfd,
 		MDSS_EVENT_PANEL_GET_ADAPTION_MODE, NULL);
-	ret = scnprintf(buf, PAGE_SIZE, "mode = %d\n"
-						"0-->Adaption Mode OFF\n"
-						"1-->Adaption Mode ON\n",
-						level);
+	ret=scnprintf(buf, PAGE_SIZE, "%d\n", level);
 	return ret;
 }
 
