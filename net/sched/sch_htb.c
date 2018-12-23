b1e4680b1e22f30e2fe76af83dae32837477e3fb
celtare21 <celtare21@gmail.com>
2018-12-23 11:58:20 +0000

    net/sched: Shut up
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- net/sched/sch_htb.c
+++ net/sched/sch_htb.c
@@ -1512,7 +1512,7 @@ static int htb_change_class(struct Qdisc *sch, u32 classid,
 	sch_tree_unlock(sch);
 
 	if (warn)
-		pr_warn("HTB: quantum of class %X is %s. Consider r2q change.\n",
+		pr_debug("HTB: quantum of class %X is %s. Consider r2q change.\n",
 			    cl->common.classid, (warn == -1 ? "small" : "big"));
 
 	qdisc_class_hash_grow(sch, &q->clhash);
