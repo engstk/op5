3b4ea70e1c44440dc62b9f4e488798faeb162f79
engstk <eng.stk@sapo.pt>
2019-05-07 23:43:46 +0000

    stune: Remove Dynamic SchedTune Boost
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- kernel/sched/boost.c
+++ kernel/sched/boost.c
@@ -103,10 +103,6 @@ enum sched_boost_policy sched_boost_policy(void)
 	return boost_policy;
 }
 
-#ifdef CONFIG_DYNAMIC_STUNE_BOOST
-static int boost_slot;
-#endif // CONFIG_DYNAMIC_STUNE_BOOST
-
 static bool verify_boost_params(int old_val, int new_val)
 {
 	/*
@@ -205,10 +201,6 @@ int sched_boost_handler(struct ctl_table *table, int write,
 
 	if (verify_boost_params(old_val, *data)) {
 		_sched_set_boost(old_val, *data);
-		if (*data > 0)
-			do_stune_sched_boost("top-app", &boost_slot);
-		else
-			reset_stune_boost("top-app", boost_slot);
 	} else {
 		*data = old_val;
 		ret = -EINVAL;
