20a4fb13aa8858184a87a64ede22e66bfd8f24e8
engstk <eng.stk@sapo.pt>
2019-10-04 12:18:26 +0100

    fs: exfat: add exfat-linux source
    from https://github.com/arter97/exfat-linux repo
    
    Signed-off-by: engstk <eng.stk@sapo.pt>

---
+++ fs/exfat/version.h
@@ -0,0 +1,29 @@
+/*
+ *  Copyright (C) 2012-2013 Samsung Electronics Co., Ltd.
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+/************************************************************************/
+/*                                                                      */
+/*  PROJECT : exFAT & FAT12/16/32 File System                           */
+/*  FILE    : version.h                                                 */
+/*  PURPOSE : exFAT File Manager                                        */
+/*                                                                      */
+/************************************************************************/
+
+#define EXFAT_BASE_VERSION	"2.2.0"
+#define EXFAT_EXTRAVERSION	"1"
+#define EXFAT_VARIANT		"arter97"
+#define EXFAT_VERSION		EXFAT_BASE_VERSION "-" EXFAT_EXTRAVERSION EXFAT_VARIANT
