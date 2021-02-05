1d341c29104d24f7f2e03d396443729de0c8df4c
engstk <eng.stk@sapo.pt>
2021-02-05 12:17:28 +0000

    Revert "script/wireguard: more reliable version selection"
    This reverts commit 956ce43fdece656485a442cccfa929c1ed346725.

--- scripts/fetch-latest-wireguard.sh
+++ scripts/fetch-latest-wireguard.sh
@@ -1,28 +1,21 @@
 #!/bin/bash
 set -e
-USER_AGENT="WireGuard-AndroidROMBuild/0.2 ($(uname -a))"
+USER_AGENT="WireGuard-AndroidROMBuild/0.1 ($(uname -a))"
 
 exec 9>.wireguard-fetch-lock
 flock -n 9 || exit 0
 
 [[ $(( $(date +%s) - $(stat -c %Y "net/wireguard/.check" 2>/dev/null || echo 0) )) -gt 86400 ]] || exit 0
 
-while read -r distro package version _; do
-	if [[ $distro == upstream && $package == kmodtools ]]; then
-		VERSION="$version"
-		break
-	fi
-done < <(curl -A "$USER_AGENT" -LSs https://build.wireguard.com/distros.txt)
+[[ $(curl -A "$USER_AGENT" -LSs https://git.zx2c4.com/WireGuard/refs/) =~ snapshot/WireGuard-([0-9.]+)\.tar\.xz ]]
 
-[[ -n $VERSION ]]
-
-if [[ -f net/wireguard/version.h && $(< net/wireguard/version.h) == *$VERSION* ]]; then
+if [[ -f net/wireguard/version.h && $(< net/wireguard/version.h) == *${BASH_REMATCH[1]}* ]]; then
 	touch net/wireguard/.check
 	exit 0
 fi
 
 rm -rf net/wireguard
 mkdir -p net/wireguard
-curl -A "$USER_AGENT" -LsS "https://git.zx2c4.com/WireGuard/snapshot/WireGuard-$VERSION.tar.xz" | tar -C "net/wireguard" -xJf - --strip-components=2 "WireGuard-$VERSION/src"
+curl -A "$USER_AGENT" -LsS "https://git.zx2c4.com/WireGuard/snapshot/WireGuard-${BASH_REMATCH[1]}.tar.xz" | tar -C "net/wireguard" -xJf - --strip-components=2 "WireGuard-${BASH_REMATCH[1]}/src"
 sed -i 's/tristate/bool/;s/default m/default y/;' net/wireguard/Kconfig
 touch net/wireguard/.check
