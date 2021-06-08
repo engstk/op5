2a82f0e9fdbc91c3bdd893d1a2db9a98b95a9b8e
Jebaitedneko <Jebaitedneko@gmail.com>
2021-06-08 08:43:10 +0530

    hwtracing: Add entries from msm8998-coresight
    cat msm8998-coresight.dtsi | grep primecell-periphid | cut -c29- | sed "s/>;//g;s/^/ETM4x_AMBA_ID(/g;s/$/),/g" | sort -u
    
    eng.stk: adapt and add clocks for msm8998
    
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- drivers/hwtracing/google/coresight-clk-amba-placeholder.c
+++ drivers/hwtracing/google/coresight-clk-amba-placeholder.c
@@ -36,6 +36,11 @@ static const struct amba_id coresight_ids[] = {
 	ETM4x_AMBA_ID(0x000bb95a), /* Cortex-A72 */
 	ETM4x_AMBA_ID(0x000bb959), /* Cortex-A73 */
 	ETM4x_AMBA_ID(0x000bb9da), /* Cortex-A35 */
+	ETM4x_AMBA_ID(0x0003b908),
+	ETM4x_AMBA_ID(0x0003b909),
+	ETM4x_AMBA_ID(0x0003b95d),
+	ETM4x_AMBA_ID(0x0003b961),
+	ETM4x_AMBA_ID(0x0003b962),
 	/* dynamic-replicator IDs */
 	{
 		.id = 0x000bb909,
