961b46a87c57d7b166ffff861e0e0c9e887dea75
engstk <eng.stk@sapo.pt>
2022-03-11 12:29:46 +0000

    Revert "selinux: don't require auditing"
    This reverts commit 7416ff5788c35f8152ab5617de631283f19cf45d.

--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -3249,7 +3249,6 @@ out:
 	return match;
 }
 
-#ifdef CONFIG_AUDIT
 static int (*aurule_callback)(void) = audit_update_lsm_rules;
 
 static int aurule_avc_callback(u32 event)
@@ -3272,7 +3271,6 @@ static int __init aurule_init(void)
 	return err;
 }
 __initcall(aurule_init);
-#endif
 
 #ifdef CONFIG_NETLABEL
 /**
